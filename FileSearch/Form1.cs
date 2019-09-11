b16a0a28647c336f0f2cedc6a61a096761bd591d
Ilshat <gubaidullin.i@gmail.com>
2019-09-11 15:00:11 +0300

    добавил паузу

--- FileSearch/Form1.cs
+++ FileSearch/Form1.cs
@@ -15,9 +15,11 @@ namespace FileSearch
     public partial class Form1 : Form
     {
 
-       // private static ManualResetEvent _stopper = new ManualResetEvent(false);
+        private static ManualResetEvent _stopper = new ManualResetEvent(false);
+        private delegate void EventHandle();
         bool _paused;
         Thread _SearchThrd;
+        string[] _FilesInDirectory;
         public Form1()
         {
             InitializeComponent();
@@ -27,34 +29,51 @@ namespace FileSearch
         {
             if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
                 txtBxDirName.Text = folderBrowserDialog1.SelectedPath;
+
         }
 
         private void btnStart_Click(object sender, EventArgs e)
         {
-            _SearchThrd = new Thread(new ThreadStart(SearchFile));
+            _FilesInDirectory = Directory.GetFiles(txtBxDirName.Text, txtBxFileName.Text, SearchOption.AllDirectories);
+            progressBar1.Maximum = _FilesInDirectory.Length;
             
+            _SearchThrd = new Thread(new ThreadStart(SearchFile));
+           
             if (txtBxSearch.Text == String.Empty) MessageBox.Show("Заполните поле Что ищем?");
             if (txtBxFileName.Text == String.Empty) MessageBox.Show("Заполните поле Шаблон файла");
 
             _paused = false;
+            button2.Text = "Остановить";
+
             if ((txtBxSearch.Text != String.Empty) && (txtBxFileName.Text != String.Empty))
                 _SearchThrd.Start();
+
+            
+            _stopper.Set();
 
         }
 
         private void SearchFile()
         {
-            string[] FilesInDirectory = Directory.GetFiles(txtBxDirName.Text, txtBxFileName.Text, SearchOption.AllDirectories);
 
-            progressBar1.Maximum = FilesInDirectory.Length;
+            var FilesInDirectory = _FilesInDirectory;
+
             var count = 0;
+                  
+            
             foreach (var f in FilesInDirectory)
             {
+                _stopper.WaitOne();
                 if (File.ReadAllText(f).Contains(txtBxSearch.Text))
-                    listBox1.Items.Add(f);
+                {
+                    listBox1.Invoke(new Action(() => listBox1.Items.Add(f)));
+                }
                 count++;
-                progressBar1.Value = count;
+                progressBar1.Invoke(new Action(() => progressBar1.Value = count));
             }
+
+          // if (count == FilesInDirectory.Length) _stopper.Close();
+            
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -63,13 +82,25 @@ namespace FileSearch
             {
                 StopSearchThread();
                 button2.Text = "Возобновить";
-
             }
+            else
+            {
+                ContinueSearchThread();
+                button2.Text = "Остановить";
+            }
+        }
+
+        private void ContinueSearchThread()
+        {
+            _stopper.Set();
+            _paused = false;
         }
 
         private void StopSearchThread()
         {
-           _SearchThrd.Sle
+            _paused = true;
+            _stopper.Reset();
+           
         }
     }
 }
