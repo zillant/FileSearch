04187785783f8721b2c79d013f1d6452de4bbee4
Ilshat <gubaidullin.i@gmail.com>
2019-09-11 17:21:28 +0300

    пытаюсь в treeView

--- FileSearch/Form1.Designer.cs
+++ FileSearch/Form1.Designer.cs
@@ -28,73 +28,36 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
-            this.panel3 = new System.Windows.Forms.Panel();
+            this.components = new System.ComponentModel.Container();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.txtBxDirName = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtBxSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBxFileName = new System.Windows.Forms.MaskedTextBox();
             this.panel2 = new System.Windows.Forms.Panel();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
-            this.panel1 = new System.Windows.Forms.Panel();
-            this.txtBxFileName = new System.Windows.Forms.MaskedTextBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.txtBxSearch = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.txtBxDirName = new System.Windows.Forms.TextBox();
-            this.listBox1 = new System.Windows.Forms.ListBox();
-            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.panel3 = new System.Windows.Forms.Panel();
             this.btnPause = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.tableLayoutPanel1.SuspendLayout();
-            this.panel3.SuspendLayout();
-            this.panel2.SuspendLayout();
+            this.lblFilename = new System.Windows.Forms.Label();
+            this.lblFileCount = new System.Windows.Forms.Label();
+            this.lblElapsedTime = new System.Windows.Forms.Label();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.treeView1 = new System.Windows.Forms.TreeView();
             this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tableLayoutPanel1
+            // timer1
             // 
-            this.tableLayoutPanel1.ColumnCount = 2;
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
-            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 1);
-            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
-            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
-            this.tableLayoutPanel1.Controls.Add(this.listBox1, 0, 2);
-            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
-            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
-            this.tableLayoutPanel1.RowCount = 3;
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.61069F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.38931F));
-            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 142F));
-            this.tableLayoutPanel1.Size = new System.Drawing.Size(458, 274);
-            this.tableLayoutPanel1.TabIndex = 0;
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.button2);
-            this.panel3.Controls.Add(this.btnPause);
-            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel3.Location = new System.Drawing.Point(232, 30);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(223, 98);
-            this.panel3.TabIndex = 2;
-            // 
-            // panel2
-            // 
-            this.panel2.Controls.Add(this.progressBar1);
-            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.panel2.Location = new System.Drawing.Point(232, 3);
-            this.panel2.Name = "panel2";
-            this.panel2.Size = new System.Drawing.Size(223, 21);
-            this.panel2.TabIndex = 1;
-            // 
-            // progressBar1
-            // 
-            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.progressBar1.Location = new System.Drawing.Point(0, 0);
-            this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(223, 21);
-            this.progressBar1.TabIndex = 0;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // panel1
             // 
@@ -109,49 +72,15 @@
             this.panel1.Location = new System.Drawing.Point(3, 3);
             this.panel1.Name = "panel1";
             this.tableLayoutPanel1.SetRowSpan(this.panel1, 2);
-            this.panel1.Size = new System.Drawing.Size(223, 125);
+            this.panel1.Size = new System.Drawing.Size(233, 144);
             this.panel1.TabIndex = 0;
             // 
-            // txtBxFileName
+            // txtBxDirName
             // 
-            this.txtBxFileName.Location = new System.Drawing.Point(10, 97);
-            this.txtBxFileName.Name = "txtBxFileName";
-            this.txtBxFileName.Size = new System.Drawing.Size(171, 20);
-            this.txtBxFileName.TabIndex = 8;
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(9, 81);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(81, 13);
-            this.label3.TabIndex = 7;
-            this.label3.Text = "Шаблон файла";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(9, 42);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(64, 13);
-            this.label2.TabIndex = 5;
-            this.label2.Text = "Что ищем?";
-            // 
-            // txtBxSearch
-            // 
-            this.txtBxSearch.Location = new System.Drawing.Point(10, 58);
-            this.txtBxSearch.Name = "txtBxSearch";
-            this.txtBxSearch.Size = new System.Drawing.Size(171, 20);
-            this.txtBxSearch.TabIndex = 3;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(9, 3);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(121, 13);
-            this.label1.TabIndex = 2;
-            this.label1.Text = "Выберите директорию";
+            this.txtBxDirName.Location = new System.Drawing.Point(10, 19);
+            this.txtBxDirName.Name = "txtBxDirName";
+            this.txtBxDirName.Size = new System.Drawing.Size(171, 20);
+            this.txtBxDirName.TabIndex = 0;
             // 
             // button1
             // 
@@ -163,26 +92,80 @@
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
-            // txtBxDirName
+            // label1
             // 
-            this.txtBxDirName.Location = new System.Drawing.Point(10, 19);
-            this.txtBxDirName.Name = "txtBxDirName";
-            this.txtBxDirName.Size = new System.Drawing.Size(171, 20);
-            this.txtBxDirName.TabIndex = 0;
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(121, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Выберите директорию";
             // 
-            // listBox1
+            // txtBxSearch
             // 
-            this.tableLayoutPanel1.SetColumnSpan(this.listBox1, 2);
-            this.listBox1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(3, 134);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(452, 137);
-            this.listBox1.TabIndex = 3;
+            this.txtBxSearch.Location = new System.Drawing.Point(10, 58);
+            this.txtBxSearch.Name = "txtBxSearch";
+            this.txtBxSearch.Size = new System.Drawing.Size(171, 20);
+            this.txtBxSearch.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Что ищем?";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Шаблон файла";
+            // 
+            // txtBxFileName
+            // 
+            this.txtBxFileName.Location = new System.Drawing.Point(10, 97);
+            this.txtBxFileName.Name = "txtBxFileName";
+            this.txtBxFileName.Size = new System.Drawing.Size(171, 20);
+            this.txtBxFileName.TabIndex = 8;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.progressBar1);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(242, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(233, 25);
+            this.panel2.TabIndex = 1;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.progressBar1.Location = new System.Drawing.Point(0, 0);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(233, 25);
+            this.progressBar1.TabIndex = 0;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.lblElapsedTime);
+            this.panel3.Controls.Add(this.lblFileCount);
+            this.panel3.Controls.Add(this.lblFilename);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.btnPause);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(242, 34);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(233, 113);
+            this.panel3.TabIndex = 2;
             // 
             // btnPause
             // 
-            this.btnPause.Location = new System.Drawing.Point(13, 15);
+            this.btnPause.Location = new System.Drawing.Point(85, 6);
             this.btnPause.Name = "btnPause";
             this.btnPause.Size = new System.Drawing.Size(75, 23);
             this.btnPause.TabIndex = 0;
@@ -192,7 +175,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(3, 49);
+            this.button2.Location = new System.Drawing.Point(76, 32);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(95, 23);
             this.button2.TabIndex = 1;
@@ -200,41 +183,98 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // lblFilename
+            // 
+            this.lblFilename.AutoSize = true;
+            this.lblFilename.Location = new System.Drawing.Point(12, 60);
+            this.lblFilename.Name = "lblFilename";
+            this.lblFilename.Size = new System.Drawing.Size(0, 13);
+            this.lblFilename.TabIndex = 2;
+            // 
+            // lblFileCount
+            // 
+            this.lblFileCount.AutoSize = true;
+            this.lblFileCount.Location = new System.Drawing.Point(3, 73);
+            this.lblFileCount.Name = "lblFileCount";
+            this.lblFileCount.Size = new System.Drawing.Size(182, 13);
+            this.lblFileCount.TabIndex = 3;
+            this.lblFileCount.Text = "Количество обработанных файлов";
+            // 
+            // lblElapsedTime
+            // 
+            this.lblElapsedTime.AutoSize = true;
+            this.lblElapsedTime.Location = new System.Drawing.Point(3, 86);
+            this.lblElapsedTime.Name = "lblElapsedTime";
+            this.lblElapsedTime.Size = new System.Drawing.Size(58, 13);
+            this.lblElapsedTime.TabIndex = 4;
+            this.lblElapsedTime.Text = "00:00:00.0";
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.panel3, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.panel2, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.treeView1, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 20.61069F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 79.38931F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 142F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(478, 293);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.tableLayoutPanel1.SetColumnSpan(this.treeView1, 2);
+            this.treeView1.Location = new System.Drawing.Point(3, 153);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(472, 137);
+            this.treeView1.TabIndex = 3;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(458, 274);
+            this.ClientSize = new System.Drawing.Size(478, 293);
             this.Controls.Add(this.tableLayoutPanel1);
             this.Name = "Form1";
             this.Text = "Поиск по файлам";
-            this.tableLayoutPanel1.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel2.ResumeLayout(false);
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
-        private System.Windows.Forms.Panel panel3;
-        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.MaskedTextBox txtBxFileName;
+        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox txtBxSearch;
-        private System.Windows.Forms.Panel panel1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox txtBxDirName;
-        private System.Windows.Forms.MaskedTextBox txtBxFileName;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.ListBox listBox1;
-        private System.Windows.Forms.ProgressBar progressBar1;
-        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
-        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label lblElapsedTime;
+        private System.Windows.Forms.Label lblFileCount;
+        private System.Windows.Forms.Label lblFilename;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TreeView treeView1;
     }
 }
 
--- FileSearch/Form1.cs
+++ FileSearch/Form1.cs
@@ -9,17 +9,20 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Threading;
+using System.Diagnostics;
 
 namespace FileSearch
 {
     public partial class Form1 : Form
     {
-
+        public IEnumerable<FileSystemEntry> FileSystemEntries { get; }
         private static ManualResetEvent _stopper = new ManualResetEvent(false);
         private delegate void EventHandle();
         bool _paused;
         Thread _SearchThrd;
         string[] _FilesInDirectory;
+        bool _isWork;
+        DateTime startTime;
         public Form1()
         {
             InitializeComponent();
@@ -34,11 +37,14 @@ namespace FileSearch
 
         private void btnStart_Click(object sender, EventArgs e)
         {
+            startTime = DateTime.Now;
+            treeView1.PathSeparator = @"\";
             _FilesInDirectory = Directory.GetFiles(txtBxDirName.Text, txtBxFileName.Text, SearchOption.AllDirectories);
             progressBar1.Maximum = _FilesInDirectory.Length;
             
             _SearchThrd = new Thread(new ThreadStart(SearchFile));
-           
+            timer1.Start();
+
             if (txtBxSearch.Text == String.Empty) MessageBox.Show("Заполните поле Что ищем?");
             if (txtBxFileName.Text == String.Empty) MessageBox.Show("Заполните поле Шаблон файла");
 
@@ -55,25 +61,59 @@ namespace FileSearch
 
         private void SearchFile()
         {
-
+            _isWork = true;
             var FilesInDirectory = _FilesInDirectory;
-
+            List<string> paths = new List<string>();
             var count = 0;
-                  
+            var root = new TreeNode();
+            treeView1.Invoke(new Action(() => treeView1.Nodes.Add(root)));
             
-            foreach (var f in FilesInDirectory)
+            try
             {
-                _stopper.WaitOne();
-                if (File.ReadAllText(f).Contains(txtBxSearch.Text))
+                foreach (var f in FilesInDirectory)
                 {
-                    listBox1.Invoke(new Action(() => listBox1.Items.Add(f)));
+                    _stopper.WaitOne();
+                    lblFilename.Invoke(new Action(() => lblFilename.Text = f));
+                    if (File.ReadAllText(f).Contains(txtBxSearch.Text))
+                    {
+                        //listBox1.Invoke(new Action(() => listBox1.Items.Add(f)));
+                        paths.Add(f);
+                        root.
+                    }
+                    count++;
+                    progressBar1.Invoke(new Action(() => progressBar1.Value = count));
+                    lblFileCount.Invoke(new Action(() => lblFileCount.Text = $"Файлов обработано {count}"));
                 }
-                count++;
-                progressBar1.Invoke(new Action(() => progressBar1.Value = count));
+
+                _isWork = false;
             }
+            catch (System.Exception excp)
+            { }
 
           // if (count == FilesInDirectory.Length) _stopper.Close();
             
+        }
+
+        private static void PopulateTreeView(TreeView treeView, IEnumerable<string> paths, char pathSeparator)
+        {
+            TreeNode lastNode = null;
+            string subPathAgg;
+            foreach (string path in paths)
+            {
+                subPathAgg = string.Empty;
+                foreach (string subPath in path.Split(pathSeparator))
+                {
+                    subPathAgg += subPath + pathSeparator;
+                    TreeNode[] nodes = treeView.Nodes.Find(subPathAgg, true);
+                    if (nodes.Length == 0)
+                        if (lastNode == null)
+                            lastNode = treeView.Nodes.Add(subPathAgg, subPath);
+                        else
+                            lastNode = lastNode.Nodes.Add(subPathAgg, subPath);
+                    else
+                        lastNode = nodes[0];
+                }
+            }
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -82,11 +122,13 @@ namespace FileSearch
             {
                 StopSearchThread();
                 button2.Text = "Возобновить";
+                _isWork = false;
             }
             else
             {
                 ContinueSearchThread();
                 button2.Text = "Остановить";
+                _isWork = true;
             }
         }
 
@@ -102,5 +144,17 @@ namespace FileSearch
             _stopper.Reset();
            
         }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            var ts = DateTime.Now - startTime;
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10);
+            if (_isWork)
+            {
+                lblElapsedTime.Invoke(new Action(() => lblElapsedTime.Text = elapsedTime));
+            }
+        }
     }
+
+
 }
